3db77c923641b3dec9bff87225bc24a3424f4c26
John Duncan <john@johnduncanphoto.com>
2018-03-17 18:03:29 +0000

    refactor

--- pangram.js
+++ pangram.js
@@ -4,15 +4,39 @@ Pangram = function(input) {
 
 
 
-Pangram.prototype.isPangram = function(input) {
-    if (this.input === '' || /[A-Z]/g.test(this.input) ) {
-        return false;
-    } else {
-        return true;
+Pangram.prototype.isPangram = function (input) {
+        console.log(input)
+        const regex = /([a-z])(?!.*\1)/g;
+        let abc = this.creatABC()
+        let str = this.input.split(" ").join("").split("");
+        str.forEach(element => {
+            this.checkLetter(str[element], abc)
+        })
+    if (abc.length === 0 ) {
+            return true
+        } else {
+            return false
+        }
+       
+    }
+    
+    Pangram.prototype.creatABC = function () {
+        var abc = "abcdefghijklmnopqrstuvwxyz"
+        abc = abc.split("")
+        return abc
+     }
+
+    Pangram.prototype.checkLetter =  function (letter, arr) {
+        arr.forEach(element => {
+            if (letter === arr[element]) {
+                arr.splice(element, 1)
+            }
+        });
     }
 
-   
-}
+
 
 
 module.exports = Pangram;
+
+
