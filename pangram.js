410fb955ce2d8a7e4cd7057e8bb34bcd4eabb5dc
John Duncan <john@johnduncanphoto.com>
2018-03-18 10:52:42 +0000

    first four test passing

--- pangram.js
+++ pangram.js
@@ -4,34 +4,30 @@ Pangram = function(input) {
 
 
 
-Pangram.prototype.isPangram = function (input) {
-        console.log(input)
-        const regex = /([a-z])(?!.*\1)/g;
-        let abc = this.creatABC()
-        let str = this.input.split(" ").join("").split("");
-        str.forEach(element => {
-            this.checkLetter(str[element], abc)
-        })
-    if (abc.length === 0 ) {
-            return true
-        } else {
-            return false
+Pangram.prototype.isPangram = function () {    
+    var regex = /([a-z])(?!.*\1)/g;
+    const letterArray =(this.input.match(regex) || []);
+    if(letterArray.length === 26) {
+        return true;
+    } else {
+        return false;
         }
-       
     }
+
+
     
-    Pangram.prototype.creatABC = function () {
+    Pangram.prototype.createABC = function () {
         var abc = "abcdefghijklmnopqrstuvwxyz"
         abc = abc.split("")
         return abc
      }
 
     Pangram.prototype.checkLetter =  function (letter, arr) {
-        arr.forEach(element => {
-            if (letter === arr[element]) {
-                arr.splice(element, 1)
+        for (let i = 0; i <= arr.length - 1; i++) {
+            if (letter === arr[i]) {
+                arr.splice(i, 1)
             }
-        });
+        }
     }
 
 
@@ -39,4 +35,25 @@ Pangram.prototype.isPangram = function (input) {
 
 module.exports = Pangram;
 
+const pangram = new Pangram('the quick brown fox jumps over the lazy dog')
 
+pangram.isPangram()
+
+// if (this.input !== '') {
+//     const regex = /([a-z])(?!.*\1)/g;
+//     const abc = this.creatABC()
+//     // abc is a split up array
+//     const str = this.input.split(" ").join("").split("");
+//     str.forEach(element => {
+//         this.checkLetter(str[element], abc)
+//         //here I am checking each letter against abc
+//     })
+
+//     if (abc.length === 0 || 26) {
+//         return true
+//     } else {
+//         return false
+//     }
+// } else {
+//     return false;
+// }
